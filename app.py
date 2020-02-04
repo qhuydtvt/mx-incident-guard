4f32b0a0641ab38f265871811b0aec19af421496
huynq.work <huynq.work@gmail.com>
2020-02-04 18:25:15 +0700

    Update conjob minute

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from dateutil.tz import gettz
 sched = BlockingScheduler()
 log = get_logger('incident-guard')
 
-@sched.scheduled_job('cron', day_of_week='0-6', timezone='asia/ho_chi_minh', hour='9-19', minute='*', second='0-59/10')
+@sched.scheduled_job('cron', day_of_week='0-6', timezone='asia/ho_chi_minh', hour='9-19', minute='0,30')
 def check_job():
   log('Checking inactive cards')
   check_in_progress_inactive_cards()
